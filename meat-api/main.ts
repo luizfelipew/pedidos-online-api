f0fd719cad8112497591898ea828a21ab5763c09
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-05-29 22:21:35 -0300

    Configurando server e environment

--- meat-api/main.ts
+++ meat-api/main.ts
@@ -1,1 +1,11 @@
-console.log('main script...');
+import {Server} from './server/server'
+import { log } from 'util';
+
+const server = new Server()
+server.bootstrap().then( server => {
+    console.log('Server is listening on:' , server.application.address())
+}).catch(error => {
+    console.log('Server failed to start ')
+    console.error(error)
+    process.exit(1)
+})
---
+++ meat-api/server/server.ts
@@ -0,0 +1,59 @@
+import * as restify from 'restify'
+import {environment} from '../common/environment'
+
+export class Server {
+    
+
+    application: restify.Server
+
+    initRoutes(): Promise<any>{
+        return new Promise((resolve, reject) => {
+            try {
+                this.application = restify.createServer({
+                    name: 'meat-api',
+                    version: '1.0.0'
+                })
+                //configurando o query parser para obter os parametros da query
+                this.application.use(restify.plugins.queryParser())
+
+                //routes
+                this.application.get('/info',[  
+                    (req, resp, next) =>{
+                        if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
+                           // resp.status(400)
+                           // resp.json({message: 'Please, update your browser'})
+                           let error: any = new Error()
+                           error.statusCode = 400
+                           error.message = 'Please, update your browser'
+                           return next(error)
+                        }
+                        return next()
+                } , (req, resp, next) => {
+                    resp.json({
+                        browser: req.userAgent(),
+                        method: req.method,
+                        url: req.href(),
+                        path: req.path(),
+                        // depois do queryparser
+                        query: req.query 
+                    })
+                    return next()
+                }])
+                
+
+                this.application.listen(environment.server.port, () => {
+                    //console.log('API is running on http://localhost:3000');    
+                    resolve(this.application)
+                })
+
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+    bootstrap(): Promise<Server>{
+        return this.initRoutes().then(()=> this)
+    }
+
+}
