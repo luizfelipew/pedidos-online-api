9b13b416e48ff709104503c8c6b24fb442943d94
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-05-31 21:07:16 -0300

    implementando get by Id

--- meat-api/users/users.router.ts
+++ meat-api/users/users.router.ts
@@ -3,6 +3,7 @@ import * as restify from 'restify'
 import {User} from './users.model'
 
 class UsersRouter extends Router {
+    
     applyRoutes(application: restify.Server){
         application.get('/users', (req, resp, next) => {
             User.findAll().then(users => {
@@ -10,7 +11,20 @@ class UsersRouter extends Router {
                 return next()
             })
         })
+
+        application.get('/users/:id', (req, resp, next) => {
+            User.finrById(req.params.id).then(user => {
+                if(user){
+                    resp.json(user)
+                    return next()
+                }
+
+                resp.send(404)
+                return next()
+            })
+        })
     }
+
 } 
 
 export const usersRouter = new UsersRouter()
